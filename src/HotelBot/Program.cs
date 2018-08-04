d0617543283ed901118b8c508fd463278c025998
Barış Ceviz <barisceviz97@hotmail.com>
2018-08-04 03:05:07 +0300

    Added run commands of timer

---
+++ src/HotelBot/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using HotelBot.Core;
+using HotelBot.Services;
+using HotelBot.Utils;
+
+namespace HotelBot
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Bot is initializing...");
+            //Initializing
+            IUrlBuilder urlBuilder = new UrlBuilder();
+            IHotelCrawler hotelCrawler = new HotelCrawler();
+            IHotelServices hotelServices = new HotelServices(hotelCrawler, urlBuilder);
+
+            Console.WriteLine("Tenda Hotel Bot is running...");
+            var timer = new HotelTimer(hotelServices);
+            timer.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
