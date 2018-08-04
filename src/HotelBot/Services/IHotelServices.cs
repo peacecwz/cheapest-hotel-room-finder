bb41f7cd51a72a5389be458fff3479b72488ad61
Barış Ceviz <barisceviz97@hotmail.com>
2018-08-04 03:04:30 +0300

    Created Hotel Service

---
+++ src/HotelBot/Services/IHotelServices.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using HotelBot.Models;
+
+namespace HotelBot.Services
+{
+    public interface IHotelServices : IDisposable
+    {
+        Task<HotelRoomModel> GetSingleRoom (DateTime startDate, DateTime endDate);
+    }
+}
