35485bd491b7a0aa12c45bfeecc241d0dab6baa2
Barış Ceviz <barisceviz97@hotmail.com>
2018-08-04 03:05:28 +0300

    Created UrlBuilder unit test

---
+++ tests/HotelBot.Tests/UrlBuilderTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelBot.Tests
+{
+    public class UrlBuilderTests
+    {
+        //Test will implement
+    }
+}
