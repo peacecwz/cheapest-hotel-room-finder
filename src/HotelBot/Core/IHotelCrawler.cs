71630b991244b05e8c1a750812027ab2ddd2be26
Barış Ceviz <barisceviz97@hotmail.com>
2018-08-04 03:03:43 +0300

    Created hotel crawler

---
+++ src/HotelBot/Core/IHotelCrawler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HotelBot.Models;
+
+namespace HotelBot.Core
+{
+    public interface IHotelCrawler : IDisposable
+    {
+        Task<List<HotelRoomModel>> GetAvailableRooms (string url);
+    }
+}
