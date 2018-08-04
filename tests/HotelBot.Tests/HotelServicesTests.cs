cf8004acac8261247f453ffa13c7050ce08efa2e
Barış Ceviz <barisceviz97@hotmail.com>
2018-08-04 03:05:38 +0300

    Created HotelServices unit test

---
+++ tests/HotelBot.Tests/HotelServicesTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelBot.Tests
+{
+    public class HotelServicesTests
+    {
+        //Test will implement
+    }
+}
