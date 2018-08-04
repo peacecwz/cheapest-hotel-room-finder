ff9bfeb8cd699b50f955d54d0e4cab75f82ed114
Barış Ceviz <barisceviz97@hotmail.com>
2018-08-04 03:03:32 +0300

    Created hotel room model

---
+++ src/HotelBot/Models/HotelRoomModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelBot.Models
+{
+    public class HotelRoomModel
+    {
+        public string Name { get; set; }
+        public int GuestCount { get; set; }
+        public decimal Price { get; set; }
+    }
+}
